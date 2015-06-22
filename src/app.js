9cbb2d0cecc74e4128edfb31174e496dfd2a01cf
Dominik Meyer <dominik.meyer2809@hotmail.de>
2015-06-23 01:53:49 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Main',
-  //icon: 'images/menu_icon.png',
-  subtitle: 'Menu',
-  body: 'Test Test Test Trolololo'
+var window = new UI.Window();
+
+var bgRect = new UI.Rect
+({
+	position:		  Vector2(10, 20),
+	size:			  Vector2(124, 60),
+	backgroundColor: 'black'
 });
 
-main.show();
+window.add(bgRect);
+
+var timeText = new UI.TimeText
+({
+	position:  Vector2(0, 24),
+	size:	   Vector2(144, 30),
+	color: 	   'white',
+	text:	   '%H:%M',
+	textAlign: 'center',
+	font:	   'bitham-42-bold'
+});
+
+window.add(timeText);
+
+window.show();
